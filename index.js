6c07b2f84bdb559dc5383cf4ec055aebcd061cef
RavinduMihiranga <ravindumihiranga788@gmail.comgit config --global user.email ravindumihiranga788@gmail.com>
2023-11-13 11:03:22 +0530

    multer and images folder added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
 const app = express();
 const port = 2061;
 
@@ -11,6 +14,24 @@ const orderRouter = require("./routers/orders.js");
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');  // Specify the upload folder here
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('productImage'), (req, res) => {
+  // Handle file upload logic here
+  res.send('File uploaded!');
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(userRouter);
 app.use(productRouter);
